d9e90f80a4be9e511213b33e4e5e495e3fffb542
slaygun <nabhraghav@gmail.com>
2024-02-05 20:05:34 +0530

    still tweaking. gradient issue

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -47,10 +47,9 @@ export default function Component() {
             </AccordionContent>
           </AccordionItem>
         </Accordion>
+      
       </div>
-      <div class="circlePosition w-full h-full bg-[#36a0d142] rounded-[100%] absolute -z-10 blur-[100px] flex justify-center items-center">
-        <div class="circle w-[17rem] h-[17rem] bg-[#26b9fd42] rounded-[100%]" />
-      </div>
+
     </section>
   )
 }
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,7 +6,7 @@ import { Linkedin } from 'lucide-react';
 
 function Footer() {
   return (
-    <div className="bg-gray-900   py-8 px-4 md:px-6">
+    <div className="bg-gray-900 py-8 px-4 md:px-6">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
         <div className="flex flex-col items-start">
           <div className="py-4 flex items-center">
